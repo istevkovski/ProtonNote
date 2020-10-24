8e1cdd8ca7e437308ac026c8c57f82d523ee0bad
Ivan Stevkovski <ivan@qbit.solutions>
2020-10-24 19:35:48 +0200

    Updates

---
+++ precache-manifest.bb4db69b7ba69a846d0d082e620264c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8415761a886e795c9786c3d5da1a8a69",
+    "url": "/istevkovski/ProtonNote/index.html"
+  },
+  {
+    "revision": "7885eb9e0c3bf2d84ba0",
+    "url": "/istevkovski/ProtonNote/static/css/main.aa1cb621.chunk.css"
+  },
+  {
+    "revision": "7d320c96955a46f21ee6",
+    "url": "/istevkovski/ProtonNote/static/js/2.b853d710.chunk.js"
+  },
+  {
+    "revision": "c539cd54423426898ababe6867a114a8",
+    "url": "/istevkovski/ProtonNote/static/js/2.b853d710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7885eb9e0c3bf2d84ba0",
+    "url": "/istevkovski/ProtonNote/static/js/main.d64c41e6.chunk.js"
+  },
+  {
+    "revision": "3b38dd6da882fa05e977",
+    "url": "/istevkovski/ProtonNote/static/js/runtime-main.ba88453f.js"
+  }
+]);
